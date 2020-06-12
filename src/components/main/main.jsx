20fb07bd74046d4f9d6164f0428e0a2d637e0b51
vadigru <vg@outlook.ie>
2020-06-12 13:12:29 +0300

    Deletes quotes in alt attribute

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -103,7 +103,7 @@ const Main = (props) => {
             return (
               <article key = {index} className="small-movie-card catalog__movies-card">
                 <div className="small-movie-card__image">
-                  <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="{movie}" width="280" height="175" />
+                  <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={movie} width="280" height="175" />
                 </div>
                 <h3 className="small-movie-card__title">
                   <a className="small-movie-card__link" href="movie-page.html">{movie}</a>
